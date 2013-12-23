f7a6e510c14656ea5835f7e68b19f1ccd87f5e95
CannibalVox <sdbaynham@gmail.com>
2013-12-23 01:19:11 -0600

    Fixed thaumcraft integration for newest version.

--- src/java/com/sirolf2009/necromancy/item/ItemNecromancy.java
+++ src/java/com/sirolf2009/necromancy/item/ItemNecromancy.java
@@ -1,8 +1,8 @@
 package com.sirolf2009.necromancy.item;
 
-import thaumcraft.api.EnumTag;
-import thaumcraft.api.ObjectTags;
 import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
 import net.minecraft.block.Block;
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.Item;
@@ -38,53 +38,53 @@ public class ItemNecromancy {
 
     public static void initItems() {
         genericItems = new ItemGeneric(ConfigurationNecromancy.GenericItemID).setUnlocalizedName("ItemNecromancy");
-        ThaumcraftApi.registerObjectTag(genericItems.itemID, 0, (new ObjectTags()).add(EnumTag.DEATH, 2));
-        ThaumcraftApi.registerObjectTag(genericItems.itemID, 1, (new ObjectTags()).add(EnumTag.FLIGHT, 2).add(EnumTag.LIFE, 4).add(EnumTag.LIGHT, 2).add(EnumTag.CRYSTAL, 2));
-        ThaumcraftApi.registerObjectTag(genericItems.itemID, 2, (new ObjectTags()).add(EnumTag.HEAL, 2).add(EnumTag.LIFE, 4).add(EnumTag.CRYSTAL, 2));
-        ThaumcraftApi.registerObjectTag(genericItems.itemID, 3, (new ObjectTags()).add(EnumTag.FLESH, 8).add(EnumTag.KNOWLEDGE, 4));
+        ThaumcraftApi.registerObjectTag(genericItems.itemID, 0, (new AspectList()).add(Aspect.DEATH, 2));
+        ThaumcraftApi.registerObjectTag(genericItems.itemID, 1, (new AspectList()).add(Aspect.FLIGHT, 2).add(Aspect.LIFE, 4).add(Aspect.LIGHT, 2).add(Aspect.CRYSTAL, 2));
+        ThaumcraftApi.registerObjectTag(genericItems.itemID, 2, (new AspectList()).add(Aspect.HEAL, 2).add(Aspect.LIFE, 4).add(Aspect.CRYSTAL, 2));
+        ThaumcraftApi.registerObjectTag(genericItems.itemID, 3, (new AspectList()).add(Aspect.FLESH, 8).add(Aspect.MIND, 4));
         for (int x = 0; x < ItemGeneric.names.length; x++) {
             LanguageRegistry.addName(new ItemStack(genericItems, 1, x), ItemGeneric.names[x]);
         }
-        
+
         necronomicon = new ItemNecronomicon(ConfigurationNecromancy.NecronomiconID).setUnlocalizedName("Necronomicon");
         LanguageRegistry.addName(necronomicon, "Necronomicon");
-        ThaumcraftApi.registerObjectTag(necronomicon.itemID, -1, (new ObjectTags()).add(EnumTag.KNOWLEDGE, 666).add(EnumTag.DEATH, 666).add(EnumTag.DARK, 666).add(EnumTag.EVIL, 666).add(EnumTag.CONTROL, 666));
-        
+        ThaumcraftApi.registerObjectTag(necronomicon.itemID, -1, (new AspectList()).add(Aspect.MIND, 666).add(Aspect.DEATH, 666).add(Aspect.DARKNESS, 666).add(Aspect.UNDEAD, 666).add(Aspect.UNDEAD, 666));
+
         scythe = new ItemScythe(ConfigurationNecromancy.ScytheID, ItemScythe.toolScythe).setUnlocalizedName("ItemScythe");
         LanguageRegistry.addName(scythe, "Blood Scythe");
-        ThaumcraftApi.registerObjectTag(scythe.itemID, -1, (new ObjectTags()).add(EnumTag.WEAPON, 8).add(EnumTag.LIFE, 2));
-        
+        ThaumcraftApi.registerObjectTag(scythe.itemID, -1, (new AspectList()).add(Aspect.WEAPON, 8).add(Aspect.LIFE, 2));
+
         scytheBone = new ItemScythe(ConfigurationNecromancy.ScytheBoneID, ItemScythe.toolScytheBone).setUnlocalizedName("ItemScytheBone");
         LanguageRegistry.addName(scytheBone, "Bone Scythe");
-        ThaumcraftApi.registerObjectTag(scytheBone.itemID, -1, (new ObjectTags()).add(EnumTag.WEAPON, 8).add(EnumTag.DEATH, 2));
-        
+        ThaumcraftApi.registerObjectTag(scytheBone.itemID, -1, (new AspectList()).add(Aspect.WEAPON, 8).add(Aspect.DEATH, 2));
+
         bucketBlood = new ItemBucketBlood(ConfigurationNecromancy.BucketBloodID, ConfigurationNecromancy.BloodID).setUnlocalizedName("BucketBlood");
         LanguageRegistry.addName(bucketBlood, "Blood Bucket");
-        ThaumcraftApi.registerObjectTag(scytheBone.itemID, -1, (new ObjectTags()).add(EnumTag.METAL, 13).add(EnumTag.HEAL, 16).add(EnumTag.LIFE, 32));
-        
+        ThaumcraftApi.registerObjectTag(scytheBone.itemID, -1, (new AspectList()).add(Aspect.METAL, 13).add(Aspect.HEAL, 16).add(Aspect.LIFE, 32));
+
         organs = new ItemOrgans(ConfigurationNecromancy.OrgansID).setUnlocalizedName("Organs");
-        ThaumcraftApi.registerObjectTag(organs.itemID, 0, (new ObjectTags()).add(EnumTag.FLESH, 8).add(EnumTag.KNOWLEDGE, 4));
-        ThaumcraftApi.registerObjectTag(organs.itemID, 1, (new ObjectTags()).add(EnumTag.FLESH, 8));
-        ThaumcraftApi.registerObjectTag(organs.itemID, 2, (new ObjectTags()).add(EnumTag.FLESH, 2));
-        ThaumcraftApi.registerObjectTag(organs.itemID, 3, (new ObjectTags()).add(EnumTag.FLESH, 8));
-        ThaumcraftApi.registerObjectTag(organs.itemID, 4, (new ObjectTags()).add(EnumTag.EVIL, 2));
+        ThaumcraftApi.registerObjectTag(organs.itemID, 0, (new AspectList()).add(Aspect.FLESH, 8).add(Aspect.MIND, 4));
+        ThaumcraftApi.registerObjectTag(organs.itemID, 1, (new AspectList()).add(Aspect.FLESH, 8));
+        ThaumcraftApi.registerObjectTag(organs.itemID, 2, (new AspectList()).add(Aspect.FLESH, 2));
+        ThaumcraftApi.registerObjectTag(organs.itemID, 3, (new AspectList()).add(Aspect.FLESH, 8));
+        ThaumcraftApi.registerObjectTag(organs.itemID, 4, (new AspectList()).add(Aspect.UNDEAD, 2));
         for (int x = 0; x < ItemOrgans.names.length; x++) {
             LanguageRegistry.addName(new ItemStack(organs, 1, x), ItemOrgans.names[x]);
         }
         NecroEntityBase.organID = organs.itemID;
-        
+
         bodyparts = new ItemBodyPart(ConfigurationNecromancy.BodyPartID).setUnlocalizedName("BodyParts");
-        ThaumcraftApi.registerObjectTag(organs.itemID, -1, (new ObjectTags()).add(EnumTag.EVIL, 2).add(EnumTag.BEAST, 2).add(EnumTag.DEATH, 8));
-        
+        ThaumcraftApi.registerObjectTag(organs.itemID, -1, (new AspectList()).add(Aspect.UNDEAD, 2).add(Aspect.BEAST, 2).add(Aspect.DEATH, 8));
+
         isaacsHead = new ItemIsaacsHead(ConfigurationNecromancy.IsaacsHeadID, isaac, Necromancy.Proxy.addArmour("Isaac"), 0);
         LanguageRegistry.addName(isaacsHead, "Isaac's Severed Head");
 
         spawner = new ItemSpawner(ConfigurationNecromancy.SpawnerID).setUnlocalizedName("NecroSpawner");
         LanguageRegistry.addName(new ItemStack(spawner, 1, 0), "Isaac's Soul Heart");
-        
+
         initRecipes();
     }
-    
+
     public static void initRecipes() {
         GameRegistry.addRecipe(new ItemStack(ItemNecromancy.necronomicon, 1), new Object[] { "LSL", "IBF", "LNL", Character.valueOf('B'), Item.book, Character.valueOf('L'), Item.leather, Character.valueOf('S'), ItemGeneric.getItemStackFromName("Jar of Blood"), Character.valueOf('I'), new ItemStack(Item.dyePowder, 1, 0), Character.valueOf('F'), Item.feather, Character.valueOf('N'), Item.netherStalkSeeds });
         GameRegistry.addRecipe(ItemGeneric.getItemStackFromName("Bone Needle"), new Object[] { "X", Character.valueOf('X'), new ItemStack(Item.dyePowder, 1, 15) });
